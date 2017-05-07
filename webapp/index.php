c1138c86ca59b262343f2d172f02231dde29ff90
Felix Maurer <felix.m@online.de>
2017-05-07 23:07:29 +0200

    Reuse existing database connection for index

--- webapp/index.php
+++ webapp/index.php
@@ -7,16 +7,12 @@
         <th class="product-price">Price</th>
     </tr>
 <?php
-    $mysqli = new mysqli("localhost", "shop", "FancyShop", "shop");
-    if ($mysqli->connect_errno) {
-        die("Connection failed: " . $mysqli->connect_error);
-    }
     if (isset($_GET["search"]) && $_GET["search"]!="") {
-        $tmp = "AND name LIKE '%" . $_GET["search"] . "%'";
+        $tmp = " AND name LIKE '%" . $_GET["search"] . "%'";
     } else {
         $tmp = "";
     }
-    $sql = "SELECT * FROM products WHERE hidden = 0 " . $tmp;
+    $sql = "SELECT * FROM products WHERE hidden = 0" . $tmp;
     if ($result = $mysqli->query($sql)) {
         while ($row = $result->fetch_object()) {
 ?>
@@ -26,6 +22,8 @@
     </tr>
 <?php
         }
+    } else {
+        echo $mysqli->error;
     }
 ?>
 </table>
